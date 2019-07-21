6aacd6322710ed7c495aefa5556d2c08c9620d20
ydtg1993 <946818508@qq.com>
2019-07-21 14:58:24 +0800

    Update dendrogram.php

--- config/dendrogram.php
+++ config/dendrogram.php
@@ -2,7 +2,6 @@
 /**
  * Copyright
  *
- * (c) Huang Yi <coodeer@163.com>
  *
  * For the full copyright and license information, please view the LICENSE
  * file that was distributed with this source code.
